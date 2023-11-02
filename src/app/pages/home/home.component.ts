7a6ab22d089bfe93e3fae845edcaefe107e1c686
AndrexMed <giovanniandrex@gmail.com>
2023-11-01 22:30:46 -0500

    FirstCommit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,64 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
+interface Task {
+  id: number | string,
+  title: string,
+  completed: boolean
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
+
 export class HomeComponent implements OnInit {
+
+  tasks: Task[] = [
+    {
+      id: "1",
+      title: "Test",
+      completed: true
+    }
+  ]
+
+  inputTask = new FormControl<string>('', {
+    validators: [Validators.required],
+    nonNullable: true
+  });
+
+  countTasks! : number
 
   constructor() { }
 
   ngOnInit(): void {
+    this.countTasks = this.getTotalTasks()
+  }
+
+  addTask(event: any) {
+    const id = this.tasks.length + 1
+    const title = event.target.value
+    const completed = true
+
+    const newTask: Task = {
+      id,
+      title,
+      completed
+    }
+    this.tasks.push(newTask)
+    this.inputTask.setValue('')
+  }
+
+  checkTasks() {
+
+  }
+
+  getTotalTasks(){
+    const taskCount = this.tasks.length
+    if (taskCount === 0){
+      return 0
+    }
+    return taskCount
   }
 
 }
